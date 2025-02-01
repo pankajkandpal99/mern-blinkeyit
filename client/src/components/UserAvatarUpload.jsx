0961eb27b964401ced2bd8273d8b9a68968c5716
pankajkandpal <kandpalpankaj34@gmail.com>
2025-02-01 19:24:04 +0530

    update

--- client/src/components/UserAvatarUpload.jsx
+++ client/src/components/UserAvatarUpload.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Axios from "../utils/axios";
+import Axios from "../utils/axios.js";
 import { summaryApi } from "../common/SummaryApi";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
--- client/src/utils/Axios.js
+++ client/src/utils/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { summaryApi } from "../common/SummaryApi";
+import { summaryApi } from "../common/SummaryApi.js";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const Axios = axios.create({
